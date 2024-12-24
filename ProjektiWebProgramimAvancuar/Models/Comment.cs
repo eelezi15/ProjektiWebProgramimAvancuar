dbd3a9aeb1219c23b8d5b840cb47fb9323ced79a
Lenovo <Lenovo@DESKTOP-JEGVOAH>
2024-12-24 10:41:19 +0100

    Add project files.

---
+++ ProjektiWebProgramimAvancuar/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjektiWebProgramimAvancuar.Models
+{
+    public class Comment
+    {
+        public Guid CommentId { get; set; }
+        public string Title { get; set; }
+
+        public Guid PostId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ProjektiWebProgramimAvancuar/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjektiWebProgramimAvancuar.Models
+{
+    public class Post
+    {
+        public Guid PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public virtual ICollection<Comment>? Comments { get; set; }
+    }
+}
